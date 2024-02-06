54e5569566be2bd71e777418ea2ae0f3c99139c4
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-02-06 13:19:07 +0100

    OK

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,8 +5,10 @@ const isAuth = async (req, res, next) => {
   const token = req.headers.authorization?.replace('Bearer ', '')
   if (!token) return next(new Error('Unauthorized, you have no token'));
   try {
-    const decoded = verifyToken(token, process.env.JWT_SECRET)
-    req.user = await User.findById(decoded.id)
+    const decoded = verifyToken(token, process.env.JWT_SECRET);
+    user = await User.findById(decoded.id);
+    user.contraseña = null;
+    req.user = user;
     console.log("Usuario autenticado como registrado");
     next()
   } catch (error) {
